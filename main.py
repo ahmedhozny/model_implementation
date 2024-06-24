34012509d0e3e87db97c9a2620bcf6d27e9f4edc
Ahmed Hosny <ahmhosny2002@gmail.com>
2024-06-24 09:06:34 +0300

    1 more change

--- main.py
+++ main.py
@@ -9,6 +9,11 @@ import tensorflow.compat.v1 as tf
 tf.disable_v2_behavior()
 
 
+class SynergyPrediction(BaseModel):
+    drug1_smiles: str
+    drug2_smiles: str
+
+
 class SideEffectsPrediction(BaseModel):
     drug1_id: int
     drug2_id: int
@@ -32,16 +37,16 @@ async def root():
 
 
 @app.post("/synergy")
-async def ddi_predict(drug1_smiles: str = Form(...), drug2_smiles: str = Form(...)):
-    drug_1 = smiles_encode(drug1_smiles)
-    drug_2 = smiles_encode(drug2_smiles)
+async def synergy(response: SynergyPrediction):
+    drug_1 = smiles_encode(response.drug1_smiles)
+    drug_2 = smiles_encode(response.drug2_smiles)
 
     res = await predict_senergy(drug_1["drug_smiles"], drug_2["drug_smiles"], drug_1["drug_fp"], drug_2["drug_fp"])
     return {"synergy_score": res}
 
 
 @app.post("/side_effects")
-async def ddi_predict(response: SideEffectsPrediction):
+async def side_effect(response: SideEffectsPrediction):
 
     res = await predict_side_effect(response.drug1_id, response.drug2_id, response.side_effect_id)
     return res
